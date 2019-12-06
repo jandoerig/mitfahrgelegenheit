4fedb819955452b1ef68f9d1a325e04f75c7dc6d
Jan Dörig <jan.doerig@bluewin.ch>
2019-12-06 16:43:52 +0100

    new project

---
+++ transportApp/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace transportApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}",
+                defaults: new { controller = "Eventübersicht", action = "Eventübersicht", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
