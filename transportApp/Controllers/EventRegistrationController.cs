600d239436ea3ecdbd89e004c80ff023e8d21665
Z-Thaddeus <thaddeus.zambellis@gmail.com>
2019-12-23 17:35:33 +0100

    FEATURE: REGISTRATION VIEW

---
+++ transportApp/Controllers/EventRegistrationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;	
+using System.Web.Mvc;
+
+namespace transportApp.Controllers
+{
+    public class EventRegistrationController : Controller
+    {
+		// GET: EventRegistration
+		[HttpGet]
+		public ActionResult Index()
+        {
+            return View();
+        }
+		[HttpPost]
+		public ActionResult Index(string eventname, string treffpunkt, string datum, string location)
+		{
+			// return Content($"Hello {Session["username"].ToString()} Password {Session["password"].ToString()}");
+			return RedirectToAction("", "eventübersicht");
+		}
+	}
+}
