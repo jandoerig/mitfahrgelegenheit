2616e01fbd666c7cd177211e0cf37774681d516a
Z-Thaddeus <thaddeus.zambellis@gmail.com>
2019-12-31 14:07:10 +0100

    FEATURE: Added better shit

--- transportApp/Controllers/RegistrationController.cs
+++ transportApp/Controllers/RegistrationController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using transportApp.Models;
 
 namespace transportApp.Controllers
 {
     public class RegistrationController : Controller
     {
         // GET: Registration
+		[HttpGet]
         public ActionResult Index()
-        {
+		{
             return View();
         }
-    }
+
+		[HttpPost]
+		public ActionResult Index(string username, string password1, string password2)
+		{
+			if (username != "" && password1 != "" && password2 != "") {
+				Console.WriteLine(username+ " " + password1 + " " + password2);
+				User User = new User(username, password1, password2);
+				UserModel.users.Add(User);
+				Session["username"] = username;
+				Session["password"] = password1;
+				return RedirectToAction("", "eventübersicht");
+			}
+			return View();
+		}
+	}
 }
